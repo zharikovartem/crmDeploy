4f70d453529de8d3a2aeb968b7611353853fd7bb
zharikovartem <42375825+zharikovartem@users.noreply.github.com>
2020-05-15 13:04:05 +0300

    init

---
+++ precache-manifest.fff4cc6b365e30122b069294e95c59c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afc8172178e8690b1fc25c2e530f02b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1dc177f08bd186c82fc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6c878c582a14feedc4d9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d1dc177f08bd186c82fc",
+    "url": "/static/js/2.0fd22908.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0fd22908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c878c582a14feedc4d9",
+    "url": "/static/js/main.7905eea5.chunk.js"
+  },
+  {
+    "revision": "3f032b1571a83b87956b",
+    "url": "/static/js/runtime-main.0a21fcc9.js"
+  }
+]);
